5881e30726eb5f9f98cdc89913a7c6b49f64c315
lovelxc <446064194@qq.com>
2022-06-28 19:17:14 +0800

    closely finish lab02

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mycs61a
 just record my learning. Dont copy!
+# Init
+You can prepare the coding environment via `python init.py`.
+Costumed init are welcomed!
--- init.py
+++ init.py
@@ -1,4 +1,50 @@
-import os, sys
+#!/usr/bin/env python
 
-def cmd_line(cmd):
-    try
+import os, sys
+nlab, nhw = 15, 10
+proj_list = ['hog', 'cats', 'ants']
+def bash_shell(bash_command):
+    try:
+        return os.popen(bash_command).read().strip()
+    except:
+        return None
+
+def delete_all():
+    if input("Please enter 'Yes': ") == 'Yes':
+        bash_shell('rm -rf Lab')
+        bash_shell('rm -rf zipfile')
+        bash_shell('rm -rf Homework')
+        bash_shell('rm -rf Project')
+
+def download():
+    if not os.path.exists('zipfile'): os.mkdir('zipfile')
+    os.chdir('zipfile')
+    for i in range(nlab):
+        if not os.path.exists('lab%02d.zip'%(i)): 
+            bash_shell("wget --no-check-certificate https://www-inst.eecs.berkeley.edu/~cs61a/sp22/lab/lab%02d/lab%02d.zip"%(i,i))
+    for i in range(nhw):
+        if not os.path.exists('hw%02d.zip'%(i)): 
+            bash_shell("wget --no-check-certificate https://www-inst.eecs.berkeley.edu/~cs61a/sp22/hw/hw%02d/hw%02d.zip"%(i,i))
+    for i in proj_list:
+        if not os.path.exists('%s.zip'%(i)): 
+            bash_shell("wget --no-check-certificate https://www-inst.eecs.berkeley.edu/~cs61a/sp22/proj/%s/%s.zip"%(i,i))
+    os.chdir('..')
+
+def unzip():
+    outpath = 'Homework'
+    if not os.path.exists(outpath): os.mkdir(outpath)
+    for i in range(nhw):
+        bash_shell("unzip -n zipfile/hw%02d -d "%(i) + outpath)
+    outpath = 'Lab'
+    if not os.path.exists(outpath): os.mkdir(outpath)
+    for i in range(nlab):
+        bash_shell("unzip -n zipfile/lab%02d -d "%(i) + outpath)
+
+def remove_zipfile():
+    bash_shell("rm -rf zipfile")
+if __name__ == '__main__':
+    # download *.zip
+    print('-'*20 + 'start' + '-'*20)
+    download()
+    unzip()
+    remove_zipfile()
