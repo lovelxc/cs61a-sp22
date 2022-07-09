add598b89c83e652d1f2256d2f9f9eb67101f958
lovelxc <446064194@qq.com>
2022-07-09 23:17:11 +0800

    finish lab10

--- Lab/lab10/lab10.scm
+++ Lab/lab10/lab10.scm
@@ -1,9 +1,29 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond 
+    ((< num1 num2) -1)
+    ((= num1 num2) 0)
+    ((> num1 num2) 1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+; high-order function
+(define (make-adder num)
+  ; (define (foo inc)
+  ;     (+ num inc)
+  ; ) ; it can't work, which not similar to Python.
+  (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (cond 
+    ((= base 1)
+     1)
+    ((= exp 1)
+     base)
+    ((= exp 0)
+     1)
+    ((even? exp)
+     (square (pow base (/ exp 2))))
+    (else
+     (* (square (pow base (quotient exp 2))) base))))
