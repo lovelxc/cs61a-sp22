c26b732ee530da9611392dc164fc59adc52e95fa
lovelxc <446064194@qq.com>
2022-07-10 13:10:25 +0800

    finish hw06

--- Homework/hw06/hw06.scm
+++ Homework/hw06/hw06.scm
@@ -1,13 +1,42 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
 
-(define (ascending? lst) 'YOUR-CODE-HERE)
+(define (ascending? lst)
+  (cond 
+    ((null? (cdr lst)) ; if list only have a elem
+     #t)
+    ((<= (car lst) (cadr lst)) ; if the firsts <= the seconds
+     (ascending? (cdr lst)))
+    (else
+     #f)))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (cond 
+    ((not (or (null? lst1) (null? lst2)))
+     (cons (car lst1)
+           (cons (car lst2)
+                 (interleave (cdr lst1) (cdr lst2)))))
+    ((null? lst1)
+     lst2)
+    ((null? lst2)
+     lst1)
+    (else
+     '())))
 
-(define (my-filter func lst) 'YOUR-CODE-HERE)
+(define (my-filter func lst)
+  (if (null? lst)
+      '()
+      (if (func (car lst))
+          (cons (car lst) (my-filter func (cdr lst)))
+          (my-filter func (cdr lst)))))
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst)
+  (if (null? lst)
+      '()
+      (let ((num (car lst)))
+        (cons num
+              (no-repeats
+               (my-filter (lambda (x) (not (= x num))) (cdr lst)))))))
