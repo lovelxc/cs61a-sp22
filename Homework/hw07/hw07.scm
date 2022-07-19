7ec676696acd34e551dced8d93a44ac118fea6b2
lovelxc <446064194@qq.com>
2022-07-19 10:09:34 +0000

    hw07 backup

--- Homework/hw07/hw07.scm
+++ Homework/hw07/hw07.scm
@@ -1,32 +1,59 @@
 (define (cadr lst) (car (cdr lst)))
 
 (define (make-kwlist1 keys values)
-  'YOUR-CODE-HERE)
+  (cons keys (cons values nil)))
 
-(define (get-keys-kwlist1 kwlist) 'YOUR-CODE-HERE)
+(define (get-keys-kwlist1 kwlist) (car kwlist))
 
 (define (get-values-kwlist1 kwlist)
-  'YOUR-CODE-HERE)
+  (cadr kwlist))
 
 (define (make-kwlist2 keys values)
-  'YOUR-CODE-HERE)
+  (if (and (null? keys) (null? keys))
+    '()
+    (cons (list (car keys) (car values)) (make-kwlist2 (cdr keys) (cdr values)))
+  ))
 
-(define (get-keys-kwlist2 kwlist) 'YOUR-CODE-HERE)
+(define (get-keys-kwlist2 kwlist)
+  (if (null? kwlist) 
+    '()
+    (cons (car (car kwlist)) (get-keys-kwlist2 (cdr kwlist)))
+  ))
 
 (define (get-values-kwlist2 kwlist)
-  'YOUR-CODE-HERE)
+  (if (null? kwlist) 
+    '()
+    (cons (cadr (car kwlist)) (get-values-kwlist2 (cdr kwlist)))
+  ))
 
 (define (add-to-kwlist kwlist key value)
-  'YOUR-CODE-HERE)
+  (make-kwlist (append (get-keys-kwlist kwlist) (list key)) (append (get-values-kwlist kwlist) (list value))))
 
 (define (get-first-from-kwlist kwlist key)
-  'YOUR-CODE-HERE)
+  (define (helper keys values key)
+    (cond 
+      ((null? keys) nil)
+      ((equal? (car keys) key) (car values))
+      (else (helper (cdr keys) (cdr values) key))
+    )
+  )
+  (helper (get-keys-kwlist kwlist) (get-values-kwlist kwlist) key)
+  )
 
 (define (prune-expr expr)
-  (define (prune-helper lst) 'YOUR-CODE-HERE)
-  'YOUR-CODE-HERE)
+  (define (prune-helper lst even) 
+  (cond ((null? lst) '())
+      (even (cons (car lst) (prune-helper (cdr lst) #f)))
+      (else (prune-helper (cdr lst) #t))
+  )
+  )
+  (cons (car expr) (prune-helper (cdr expr) #t)))
 
-(define (curry-cook formals body) 'YOUR-CODE-HERE)
+; apply macro
+(define (curry-cook formals body) 
+  (define-macro (thunkify expr) '(lambda (x) ,(eval expr)))
+  (thunkify  body)
+)
 
 (define (curry-consume curries args)
   'YOUR-CODE-HERE)
